7ef17aa0881165e664fc5417515b45690c46387f
fedochet <fedochet@yandex.ru>
2016-07-24 13:01:22 +0300

    add AddPostServlet and add_post.jsp

---
+++ src/main/java/controllers/AddPostServlet.java
@@ -0,0 +1,53 @@
+package controllers;
+
+import dao.interfaces.PostDAO;
+import listeners.ServicesProvider;
+import model.Post;
+import model.PostPrivacyType;
+import model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by roman on 24.07.2016.
+ */
+
+@WebServlet(urlPatterns = "/addpost")
+public class AddPostServlet extends HttpServlet {
+    PostDAO postDAO;
+
+    @Override
+    public void init() throws ServletException {
+        postDAO = (PostDAO) getServletContext().getAttribute(ServicesProvider.POST_DAO);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("add_post.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = (User) req.getSession().getAttribute("sessionUser");
+
+        String text = req.getParameter("text");
+        PostPrivacyType privacyType = PostPrivacyType.getTypeByID(Integer.parseInt(req.getParameter("post_privacy_type")));
+
+        try {
+            Post post = new Post();
+            post.setText(text);
+            post.setAuthorId(user.getId());
+            post.setPostPrivacyType(privacyType);
+
+            postDAO.create(post);
+            resp.sendRedirect("/");
+        } catch (RuntimeException e) {
+            resp.sendError(500, "Error while saving post");
+        }
+    }
+}
---
+++ src/main/webapp/add_post.jsp
@@ -0,0 +1,27 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: roman
+  Date: 24.07.2016
+  Time: 10:44
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page import="model.PostPrivacyType"%>
+<html>
+<head>
+    <title>Add new post!</title>
+</head>
+<body>
+<form action="addpost" method="post">
+    <textarea name="text" rows="10" cols="30" required></textarea><br>
+    <select name="post_privacy_type" required>
+        <c:forEach items="<%=PostPrivacyType.values()%>" var="entry">
+            <option value="${entry.id}">${entry.name()}</option>
+        </c:forEach>
+    </select>
+    <input type="submit">
+</form>
+
+</body>
+</html>
