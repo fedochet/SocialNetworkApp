f620e24095a3d5e139989cce952ef2cf16c621d2
fedochet <fedochet@yandex.ru>
2016-07-31 00:54:35 +0300

    add EncodingFilter to encode all requests to UTF-8

---
+++ src/main/java/filters/EncodingFilter.java
@@ -0,0 +1,27 @@
+package filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * Created by roman on 31.07.2016.
+ */
+@WebFilter(urlPatterns = "/*")
+public class EncodingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
