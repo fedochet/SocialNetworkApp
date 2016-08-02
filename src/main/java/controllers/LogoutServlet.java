36ce9697887829522e0b870c87d31f8facbd74ce
fedochet <fedochet@yandex.ru>
2016-08-02 14:15:07 +0300

    add logging to LogoutServlet

--- src/main/java/controllers/LogoutServlet.java
+++ src/main/java/controllers/LogoutServlet.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import lombok.extern.slf4j.Slf4j;
+import model.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,18 +14,22 @@ import java.io.IOException;
  * Created by roman on 19.07.2016.
  */
 
+@Slf4j
 @WebServlet(urlPatterns = "/logout")
 public class LogoutServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("Serving GET on {} path", req.getServletPath());
+
+        User sessionUser = (User) req.getSession().getAttribute("sessionUser");
+        if (sessionUser!=null) {
+            log.info("User with '{}' username logged out; redirecting to landing page", sessionUser.getUsername());
+        } else {
+            log.info("No user were attached to session; redirection to landing page");
+        }
+
         req.getSession().removeAttribute("sessionUser");
-
         resp.sendRedirect("/");
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req, resp);
     }
 }
