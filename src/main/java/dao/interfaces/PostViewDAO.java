386aa351a9bb65677d5403edf03ebbfc3c8ec582
fedochet <fedochet@yandex.ru>
2016-08-07 02:42:32 +0300

    add PostViewDAO

---
+++ src/main/java/dao/interfaces/PostViewDAO.java
@@ -0,0 +1,12 @@
+package dao.interfaces;
+
+import model.PostView;
+
+import java.util.List;
+
+/**
+ * Created by roman on 07.08.2016.
+ */
+public interface PostViewDAO {
+    List<PostView> getAsUserByAuthorId(int userId, int authorId, int offsetId, int limit);
+}
